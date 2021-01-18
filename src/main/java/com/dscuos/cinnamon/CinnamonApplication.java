59800fad98ec7e00bc86c4e613786b45b1fd6241
sheele41 <bbang0_0@daum.net>
2021-01-18 22:31:50 +0900

    [CIN-27] Boilerplate

---
+++ src/main/java/com/dscuos/cinnamon/CinnamonApplication.java
@@ -0,0 +1,13 @@
+package com.dscuos.cinnamon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinnamonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinnamonApplication.class, args);
+	}
+
+}
